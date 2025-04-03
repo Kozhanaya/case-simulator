022fa5f90e601428ec7d05b6286d2f9a1fbe9df6
Kozhanaya <jt37582@gmail.com>
2025-04-03 16:05:36 +0500

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts">
+<script setup>
+import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
+  <TheHeader />
+
+  <main>
+    <div class="h-100">
+      <h1>main</h1>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="header">
+      <div class="header__content">
+        <div class="header__logo">
+          <div class="header__logo-icon">
+            <img :src="'/logo.svg'" alt="кейс-симулятор">
+          </div>
+          <p class="header__logo-name">кейс-<br>симулятор</p>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  &__content {
+    padding: 1rem 20px;
+  }
+
+  &__logo {
+    display: flex;
+    align-items: start;
+    gap: 12px;
+
+    &-name {
+      font-weight: 500;
+      font-size: 20px;
+      color: #E9E9E9;
+      line-height: 110%;
+      margin: 11px 0;
+    }
+  }
+}
+</style>
